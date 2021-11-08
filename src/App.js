090edb3776357b911f31b7fdfa80e57927679552
Ghadeerrr <91834143+Ghadeerrr@users.noreply.github.com>
2021-11-08 06:19:37 +0300

    Header,adv

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-// import the required components 
+// import the required components
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 import Login from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import ProductDetails from "./components/ProductDetails";
 import WishList from "./components/WishList";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import AdvertisingSlides from "./components/AdvertisingSlides";
 
 function App() {
- 
-  return(  
+  return (
     <BrowserRouter>
-    <div>
-    <Routes>
-          <Route path="/" element={<Home />} />          
+      <div>
+        <Navbar />
+        <AdvertisingSlides />
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/Cart" element={<Cart />} />
           <Route path="/ProductDetails/:id" element={<ProductDetails />} />
           <Route path="/WishList" element={<WishList />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/SignUp" element={<SignUp />} />  
-    </Routes>
-    </div>    
-   </BrowserRouter>
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
- 
 }
 
 export default App;
-
--- src/components/AdvertisingSlides.js
+++ src/components/AdvertisingSlides.js
@@ -1,14 +1,41 @@
+import { Carousel } from "react-bootstrap";
+
 function AdvertisingSlides() {
- 
-    return(  
-     <div>
-      
-   
-     </div>
-      
-    
-    );
-   
-  }
-  
-  export default AdvertisingSlides;
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 "
+            src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/pastel-sale-email-header-design-template-2bc431832ab65e24c27d1ed7e7283299_screen.jpg?ts=1561542096"
+            height="500"
+            alt="First slide"
+          />
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.pinimg.com/564x/5a/65/61/5a6561119db533beb718347ff9c8b81d.jpg"
+            height="500"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.pinimg.com/564x/38/c6/1a/38c61a7448dfb816003f743f0a8d6e83.jpg"
+            height="500"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption></Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
+
+export default AdvertisingSlides;
