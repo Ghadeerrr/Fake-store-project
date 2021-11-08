6b58d104add73c2223ea57f732fc1fc08b1cad5e
Ghadeerrr <91834143+Ghadeerrr@users.noreply.github.com>
2021-11-08 11:27:09 +0300

    Catog

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,41 +1,44 @@
 import { useEffect } from "react";
 import axios from "axios";
 import { useDispatch } from "react-redux";
-import { setloginDetails } from "../reducers/Login/login";
-import './components.css';
+import { setLoginDetails } from "../reducers/Login/login";
+import "./components.css";
 function LogIn() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-
-    axios.get('https://fakestoreapi.com/users?limit=3').then(function (response) {
-      console.log(response.data);
-      const action = setloginDetails(response.data);
-      dispatch(action);
-    }).catch(function (error) {
-      console.error(error);
-    });
-  },[]);
-    return(  
-     <div>
-      <label><b>Username or email address</b></label>
+    axios
+      .get("https://fakestoreapi.com/users?limit=3")
+      .then(function (response) {
+        console.log(response.data);
+        const action = setLoginDetails(response.data);
+        dispatch(action);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  return (
+    <div>
+      <label>
+        <b>Username or email address</b>
+      </label>
       <input />
-      <label><b>Password</b></label>
-      <input/>
+      <label>
+        <b>Password</b>
+      </label>
+      <input />
       <button>Login</button>
       <label>
-        <input/> Remember me</label>
-      <div >
-      <span>Forgot <a href="#">password?</a></span>
+        <input /> Remember me
+      </label>
+      <div>
+        <span>
+          Forgot <a href="#">password?</a>
+        </span>
       </div>
+    </div>
+  );
+}
 
-    
-   
-     </div>
-      
-    
-    );
-   
-  }
-  
-  export default LogIn;
+export default LogIn;
