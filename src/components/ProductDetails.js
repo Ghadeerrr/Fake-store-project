c483510c323fff7a3be27324e2b128a50835f657
Ghadeerrr <91834143+Ghadeerrr@users.noreply.github.com>
2021-11-09 14:48:12 +0300

    Navbar

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,9 @@
 import { useParams, useHistory } from "react-router";
 import { useSelector } from "react-redux";
 import { Col, Row, Container, Card } from "react-bootstrap";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import NavbarAll from "./NavbarAll";
+
 function ProductDetails() {
   const { id } = useParams();
   const { filter } = useSelector((state) => state.products);
@@ -17,41 +20,49 @@ function ProductDetails() {
   //   </Card>
   // </div>
   return (
-    <Card.Body>
-      <Container>
-        <Row>
-          <Col>
-            <img src={filter[id].image} height="500px" width="500px" />
-          </Col>
-          <Col>
-            <div>
-              <h4 className="text-uppercase text-black-60">
-                {filter[id].category}
-              </h4>
+    <div>
+      <NavbarAll />
 
-              <h1 className=" text-black-20">{filter[id].title}</h1>
-            </div>
-            <p className="lead">
-              Rating {filter[id].rating && filter[id].rating.rate}
-              <i class="bi bi-star-fill"></i>
-            </p>
-            <div className="display-6 fw-bold my-4">
-              {filter[id].price + "$"}
-            </div>
-            <p className="lead">{filter[id].description}</p>
-            <button className="btn btn-outline-dark">Add to cart</button>
-          </Col>
-        </Row>
+      <Card.Body>
+        <Container>
+          <Row>
+            <Col>
+              <img src={filter[id].image} height="500px" width="500px" />
+            </Col>
+            <Col>
+              <div>
+                <h4 className="text-uppercase text-black-60">
+                  {filter[id].category}
+                </h4>
 
-        {/* <Card.Body>
+                <h1 className=" text-black-20">{filter[id].title}</h1>
+              </div>
+
+              <p className="lead">
+                Rating {filter[id].rating && filter[id].rating.rate}
+                <i class="bi bi-star-fill"></i>
+              </p>
+              <div className="display-6 fw-bold my-4">
+                {filter[id].price + "$"}
+              </div>
+              <p className="lead">{filter[id].description}</p>
+
+              <button className="btn btn-outline-dark">
+                Add to cart <AddShoppingCartIcon />
+              </button>
+            </Col>
+          </Row>
+
+          {/* <Card.Body>
                 <Card.Title>{filter[id].title}</Card.Title>
                 <Card.Title>{filter[id].category}</Card.Title>
                 <Card.Text>{filter[id].description}</Card.Text>
                 <Card.Title>{filter[id].price + "$"}</Card.Title>
               </Card.Body>
           </Card> */}
-      </Container>
-    </Card.Body>
+        </Container>
+      </Card.Body>
+    </div>
   );
 }
 
