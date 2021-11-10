1b6c334d6e50f372a0838f3d8abe67a38529f247
Ghadeerrr <91834143+Ghadeerrr@users.noreply.github.com>
2021-11-10 14:44:41 +0300

    Sig

--- src/App.js
+++ src/App.js
@@ -7,8 +7,25 @@ import ProductDetails from "./components/ProductDetails";
 import WishList from "./components/WishList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setLoginDetails } from "./reducers/Login/login";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/users?limit=3")
+      .then(function (response) {
+        console.log(response.data);
+        const action = setLoginDetails(response.data);
+        dispatch(action);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
   return (
     <BrowserRouter>
       <div>
@@ -18,7 +35,6 @@ function App() {
           <Route path="/productdetails/:id" element={<ProductDetails />} />
           <Route path="/wishList" element={<WishList />} />
           <Route path="/login" element={<Login />} />
-
           <Route path="/signUp" element={<SignUp />} />
         </Routes>
       </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,49 +1,125 @@
+import NavbarAll from "./NavbarAll";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoginDetails } from "../reducers/Login/login";
 function SignUp() {
+  const state = useSelector((state) => {
+    return {
+      loginDetails: state.loginDetails.loginDetails,
+    };
+  });
+  console.log(state.loginDetails);
+  const dispatch = useDispatch();
+  let firstName;
+  let lastName;
+  let userName;
+  let email;
+  let password;
+
+  const changeFirstName = (e) => {
+    firstName = e.target.value;
+  };
+  const changeLastName = (e) => {
+    lastName = e.target.value;
+  };
+  const changeUserName = (e) => {
+    userName = e.target.value;
+  };
+  const changeEmail = (e) => {
+    email = e.target.value;
+  };
+  const changePassword = (e) => {
+    password = e.target.value;
+  };
+  const Cheack = () => {
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < state.loginDetails.length; i++) {
+      if (state.loginDetails[i].email == email) {
+        x = 1;
+        alert("The email is taken");
+        break;
+      }
+      if (state.loginDetails[i].username == userName) {
+        y = 1;
+        alert("The username is taken");
+        break;
+      }
+    }
+    if (x == 0 && y == 0) {
+      let newUser = {
+        email: email,
+        username: userName,
+        password: password,
+        name: { firstname: firstName, lastname: lastName },
+      };
+      let arr = state.loginDetails.slice();
+      console.log(arr);
+      arr.push(newUser);
+      console.log(arr);
+      const action = setLoginDetails(arr);
+      dispatch(action);
+    }
+  };
+
   return (
     <form>
+      {" "}
+      <NavbarAll />
       <h3>Sign Up</h3>
-
       <div className="form-group">
-        <label>First name</label>
-        <input type="text" className="form-control" placeholder="First name" />
+        <label>first name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First name"
+          onChange={changeFirstName}
+        />
       </div>
-
       <div className="form-group">
         <label>Last name</label>
-        <input type="text" className="form-control" placeholder="Last name" />
+        <input
+          type="text"
+          className="form-control"
+          placeholder="First name"
+          onChange={changeLastName}
+        />
       </div>
-
       <div className="form-group">
-        <label>Email address : </label>
+        <label>Email address </label>
         <input
           type="email"
           className="form-control"
           placeholder="Enter email"
+          onChange={changeEmail}
         />
       </div>
       <div className="form-group">
-        <label>User name : </label>
+        <label>User name </label>
         <input
-          type="email"
+          type="text"
           className="form-control"
-          placeholder="Enter email"
+          placeholder="Enter username"
+          onChange={changeUserName}
         />
       </div>
-
       <div className="form-group">
         <label>Password</label>
         <input
           type="password"
           className="form-control"
           placeholder="Enter password"
+          onChange={changePassword}
         />
       </div>
-
-      <button type="submit" className="btn btn-primary btn-block">
+      <button
+        onClick={Cheack}
+        type="submit"
+        className="btn btn-primary btn-block"
+      >
         Sign Up
       </button>
       <p className="forgot-password text-right">
-        Already registered <a href="/login">Log in?</a>
+        Already registered ? <a href="/login">Log in</a>
       </p>
     </form>
   );
