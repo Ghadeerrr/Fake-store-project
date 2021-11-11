1cf976a897a36b1694b1f80d4efeb6471ebf27fe
AbdullahAlhussein <abdullah30alhussein@gmail.com>
2021-11-11 10:10:56 +0300

    WishListEle

--- src/components/WishListEle.js
+++ src/components/WishListEle.js
@@ -46,37 +46,59 @@ function WishListEle({ ele, index }) {
     //   </Card>
     // </div>
 
-    <div className="row">
-      <div className="row">
-        <div className="cart-item d-md-flex justify-content-between">
-          <div className="px-20 my-50">
-            <div className="cart-item-product-thumb">
-              <img src={ele.image} />
-            </div>
-            <div className="cart-item-product-info">
-              <h4 className="cart-item-product-title">{ele.title}</h4>
-              <div className="text-lg text-body font-weight-medium pb-1">
-                {ele.price + "$"}
-                <br />
-                <NavLink
-                  to={`/ProductDetails/${index}`}
-                  class="btn btn-outline-dark"
-                >
-                  Details
-                </NavLink>
-              </div>
-              <br />
-              <button
+    <div className="container">
+      <div className="row justify-content-around">
+
+
+         <div className=" my-5 py-4 Sh-card"  style={{width: "18rem"}}>
+                <img className="card-img-top iamge-prodect " src={ele.image}  alt="image item"/>
+                    <div className="card-body text-center">
+                        <h4 className="card-title title-discription">{ele.title}</h4>
+                        <p className="lead">{ele.price + "$"}</p>
+                        <button
                 class="btn btn-outline-dark"
                 onClick={() => deleteWishList(ele)}
               >
                 Delete from Wish List
               </button>
-            </div>
+                     
+                    </div>
           </div>
-        </div>
+
       </div>
-    </div>
+     </div>
+
+    // <div className="row">
+    //   <div className="row">
+    //     <div className="cart-item d-md-flex justify-content-between">
+    //       <div className="px-20 my-50">
+    //         <div className="cart-item-product-thumb">
+    //           <img src={ele.image} />
+    //         </div>
+    //         <div className="cart-item-product-info">
+    //           <h4 className="cart-item-product-title">{ele.title}</h4>
+    //           <div className="text-lg text-body font-weight-medium pb-1">
+    //             {ele.price + "$"}
+    //             <br />
+    //             <NavLink
+    //               to={`/ProductDetails/${index}`}
+    //               class="btn btn-outline-dark"
+    //             >
+    //               Details
+    //             </NavLink>
+    //           </div>
+    //           <br />
+    //           <button
+    //             class="btn btn-outline-dark"
+    //             onClick={() => deleteWishList(ele)}
+    //           >
+    //             Delete from Wish List
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
