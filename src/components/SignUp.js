e1934f4ce2029f9a5e7541baa5b817ceac0470c2
Ghadeerrr <91834143+Ghadeerrr@users.noreply.github.com>
2021-11-11 04:51:17 +0300

    Login.sigin

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,3 @@
-import NavbarAll from "./NavbarAll";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoginDetails } from "../reducers/Login/login";
 import { setWishList } from "../reducers/wishList/wishList";
@@ -7,13 +6,16 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { setUserName, setId } from "../reducers/Login/login";
 
+import CloseIcon from "@material-ui/icons/Close";
+import "./Login.css";
+
 function SignUp() {
   const navigate = useNavigate();
   const state = useSelector((state) => {
     return {
       wishListUsers: state.wishList.wishListUsers,
       loginDetails: state.loginDetails.loginDetails,
-      cartUsers: state.Cart.cartUsers
+      cartUsers: state.Cart.cartUsers,
     };
   });
   console.log(state.loginDetails);
@@ -42,7 +44,7 @@ function SignUp() {
   const Cheack = (e) => {
     e.preventDefault();
     console.log("check");
-    
+
     let x = 0;
     let y = 0;
     for (let i = 0; i < state.loginDetails.length; i++) {
@@ -58,96 +60,101 @@ function SignUp() {
       }
     }
     if (x == 0 && y == 0) {
-      let newUser = {id:state.loginDetails.length+1,
+      let newUser = {
+        id: state.loginDetails.length + 1,
         email: email,
         username: userName,
         password: password,
-        name: { firstname: firstName, lastname: lastName }
+        name: { firstname: firstName, lastname: lastName },
       };
       let arr = state.loginDetails.slice();
       console.log(arr);
       arr.push(newUser);
       console.log(arr);
       const action = setLoginDetails(arr);
-      dispatch(action); 
-      let arrW= state.wishListUsers.slice();
-      arrW.push({id: state.loginDetails.length+1,wishList:[]})
+      dispatch(action);
+      let arrW = state.wishListUsers.slice();
+      arrW.push({ id: state.loginDetails.length + 1, wishList: [] });
       const action2 = setWishList(arrW);
       dispatch(action2);
       const action4 = setUserName(userName);
-            dispatch(action4);
-            const action5 = setId(state.loginDetails.length+1);
-            dispatch(action5);
-      let arrc= state.cartUsers.slice();
-      arrc.push({id: state.loginDetails.length+1,cart:[]})
+      dispatch(action4);
+      const action5 = setId(state.loginDetails.length + 1);
+      dispatch(action5);
+      let arrc = state.cartUsers.slice();
+      arrc.push({ id: state.loginDetails.length + 1, cart: [] });
       const action3 = setCartUsers(arrc);
-      dispatch(action3);  
+      dispatch(action3);
       console.log(state.loginDetails);
       navigate("/");
     }
   };
 
   return (
-    <form>
-      {" "}
-      <NavbarAll />
-      <h3>Sign Up</h3>
-      <div className="form-group">
-        <label>first name</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="First name"
-          onChange={changeFirstName}
-        />
-      </div>
-      <div className="form-group">
-        <label>Last name</label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="First name"
-          onChange={changeLastName}
-        />
-      </div>
-      <div className="form-group">
-        <label>Email address </label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-          onChange={changeEmail}
-        />
-      </div>
-      <div className="form-group">
-        <label>User name </label>
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Enter username"
-          onChange={changeUserName}
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-          onChange={changePassword}
-        />
-      </div>
-      <button
-      type="submit"
-        onClick={(e) =>Cheack(e)}
-        className="btn btn-primary btn-block"
-      >
-        Sign Up
-      </button>
-      <p className="forgot-password text-right">
-        Already registered ? <Link to="/login"><p>Log in</p></Link>
-      </p>
-    </form>
+    <div className="background">
+      <form>
+        <div className="container-S">
+          <div className="Icon-x-m">
+            <Link to="/">
+              <CloseIcon className="Icon-x" />
+            </Link>
+          </div>
+          <h2 className="title-b">Sign Up</h2>
+          <div className="form-group">
+            <label>first name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+              onChange={changeFirstName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Last name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="First name"
+              onChange={changeLastName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Email address </label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={changeEmail}
+            />
+          </div>
+          <div className="form-group">
+            <label>User name </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter username"
+              onChange={changeUserName}
+            />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={changePassword}
+            />
+          </div>{" "}
+          <br />
+          <button type="submit" onClick={(e) => Cheack(e)} className="button-b">
+            Sign Up
+          </button>
+          <p className="forgot-password text-right">
+            Already registered ?<Link to="/login">Log in</Link>
+          </p>
+        </div>
+      </form>
+    </div>
   );
 }
 
